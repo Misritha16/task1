d0ce24d9be899ce459f7c29d7c4890f34f90e1f9
Y20ACS457 <Y20ACS457@RP21SYS20.win2k18.local>
2023-11-11 09:39:24 +0530

    commited

---
+++ src/main/java/com/dbc/misri/App.java
@@ -0,0 +1,13 @@
+package com.dbc.misri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
